d43f0485a3abb8ef55ca418631b2a7343ce2ef9f
henrybolster <henrybolster@gmail.com>
2018-04-21 16:52:01 -0500

    adds all state address and law info

---
+++ info.js
@@ -0,0 +1,508 @@
+foiInfo = {
+	AL: {
+		address1: "",
+		address2: "",
+		address3: "",
+		address4: "",
+		law: "Alabama Open Records Law § 36-12-40 et seq.",
+		otherText: "",
+		form: "http://www.doc.state.al.us/docs/Form023APublicRecordsRequestForm.pdf"
+	},
+	AK: {
+		address1: "Office of the Commissioner",
+		address2: "Alaska Department of Corrections",
+		address3: "Box 112000",
+		address4: "Juneau, Alaska 99811-2000",
+		law: "Alaska Open Records Law, A.S. Code § 40-25-110 et seq.",
+		otherText: "Please respond to this request in a reasonable time period.  If access to the records I am requesting will take longer, please contact me with information about when I might expect copies or the ability to inspect the requested records. If you deny any or all of this request, please cite each specific exemption you feel justifies the refusal to release the information and notify me of the appeal procedures available to me under the law."
+	},
+	AZ: {
+		address1: "",
+		address2: "",
+		address3: "",
+		address4: "",
+		law: "",
+		otherText: "",
+		form: "https://arizonadoc.mycusthelp.com/WEBAPP/_rs/(S(rwoiibv3kwiz4quapxhkrjnd))/supporthome.aspx"
+	},
+	AR: {
+		address1: "Arkansas Department of Correction",
+		address2: "P.O. Box 9707",
+		address3: "Pine Bluff, AR 71611-8707",
+		address4: "",
+		law: "Arkansas Freedom of Information Act § 25-19-101 et seq.",
+		otherText: ""	
+	},
+	CA: {
+		address1: "Office of Legal Affairs",
+		address2: "PO Box 942883",
+		address3: "Sacramento, CA 94283-001",
+		address4: "",
+		law: "Calif. Public Records Act: Gov't Code  §6250-6268",
+		otherText: ""	
+	},
+	CO: {
+		address1: "Public Information Officer",
+		address2: "Colorado Department of Corrections",
+		address3: "2862 S. Circle Drive",
+		address4: "Colorado Springs, CO 80906",
+		law: "Colorado Open Records Act § 24-72-201 C.R.S.",
+		otherText: ""	
+	},
+	CT: {
+		address1: "Freedom of Information Unit",
+		address2: "Connecticut Department of Correction",
+		address3: "24 Wolcott Hill Road",
+		address4: "Wethersfield, Conneticut 06109",
+		law: "Connecticut General Statute § 1-200 et seq.",
+		otherText: ""	
+	},
+	DC: {
+
+	},
+	DE: {
+		address1: "",
+		address2: "",
+		address3: "",
+		address4: "",
+		law: "",
+		otherText: "",
+		form: "http://www.law.umich.edu/special/policyclearinghouse/Documents/Delaware%20Data%20Request%20Form.pdf"
+	},
+	FL: {
+		address1: "",
+		address2: "",
+		address3: "",
+		address4: "",
+		law: "Article I, section 24 of the Florida Constitution, and chapter 119, F.S",
+		otherText: "",
+		email: "co.publicrecords@fdc.myflorida.com"
+	},
+	GA: {
+		address1: "",
+		address2: "",
+		address3: "",
+		address4: "",
+		law: "Georgia Open Records Act §50-18-70",
+		otherText: "",
+		email: "open.records@gdc.ga.gov"
+	},
+	HI: {
+		address1: "Department of Public Safety",
+		address2: "919 Ala Moana Boulevard, 4th floor",
+		address3: "Honolulu Hawaii 96814",
+		address4: "",
+		law: "Hawaii Sunshine Law Haw. Rev. Stat. §92.1 et seq.",
+		otherText: ""	
+	},
+	ID: {
+		address1: "",
+		address2: "",
+		address3: "",
+		address4: "",
+		law: "Idaho Public Records Act § 74-101 et seq.",
+		otherText: "",
+		email: "publicrecord@idoc.idaho.gov"
+	},
+	IL: {
+		address1: "Freedom of Information Act Officer Lisa Weitekampt",
+		address2: "Illinois Department of Corrections 1301 Concordia Court",
+		address3: "PO Box 19277",
+		address4: "Springfield, IL 62794-9277",
+		law: "Freedom of Information Act 5 ILCS 140/1 et seq.",
+		otherText: ""	
+	},
+	IN: {
+		address1: "Records Division",
+		address2: "Indiana Department of Correction",
+		address3: "302 W. Washington Street, Room E-334",
+		address4: "Indianapolis, IN 46204",
+		law: "Indiana Access to Public Records Act § 5-14-3-1 et seq.",
+		otherText: ""	
+	},
+	IA: {
+		address1: "Director, Iowa Department of Corrections",
+		address2: "Jessie M. Parker State Office Building",
+		address3: "510 East 12th street",
+		address4: "Des Moines, IA 50319",
+		law: "Iowa Open Records Law Iowa code §22.1 et seq.",
+		otherText: ""	
+	},
+	KS: {
+		address1: "Todd Fertig, Communications Director",
+		address2: "Kansas Department of Corrections 714",
+		address3: "S.W. Jackson, Suite. 300",
+		address4: "Topeka, Kansas 66603",
+		law: "Kansas Open Records Act K.S.A. 45-215 through 45-223",
+		otherText: ""	
+	},
+	KY: {
+		address1: "Open Records Request Coordinator",
+		address2: "P.O. Box 2400",
+		address3: "Frankfort, Kentucky 40602",
+		address4: "",
+		law: "Kentucky Open Records Law KRS 61. 870 (2) et seq.",
+		otherText: ""	
+	},
+	LA: {
+		address1: "Louisiana Department of Public Safety and Corrections",
+		address2: "Attn: Deputy Secretary’s Office",
+		address3: "P.O. Box 94304",
+		address4: "Baton Rouge, LA 70804",
+		law: "La.R.S. 44:1 et seq.",
+		otherText: ""	
+	},
+	ME: {
+		address1: "Maine Department of Corrections Central Office",
+		address2: "25 Tyson Drive 3rd Floor",
+		address3: "State House Station 111",
+		address4: "",
+		law: "Maine Freedom of Access Act § 402 et seq.",
+		otherText: ""	
+	},
+	MD: {
+		address1: "Renata Seergae",
+		address2: "Public Information Officer",
+		address3: "300 E. Joppa Road Suite 1000",
+		address4: "Townson, Maryland 21286-3020",
+		law: "Maryland Public Information Act §§ 10-611 et seq.",
+		otherText: ""	
+	},
+	MA: {
+		address1: "",
+		address2: "",
+		address3: "",
+		address4: "",
+		law: "",
+		otherText: "",
+		form: "http://www.mass.gov/eopss/agencies/doc/public-records-requests.html"
+	},
+	MI: {
+		address1: "FOIA Coordinator",
+		address2: "Michigan Department of Corrections",
+		address3: "P.O. Box 30003",
+		address4: "Lansing, MI 48909",
+		law: "Michigan Freedom of Information Act, Act 442 of 1976, M.C.L. § 15.231 et seq.",
+		otherText: ""	
+	},
+	MN: {
+		address1: "",
+		address2: "",
+		address3: "",
+		address4: "",
+		law: "",
+		otherText: "",
+		form: "https://mn.gov/doc/contact/data-requests/"
+	},
+	MS: {
+		address1: "Mississippi Department of Corrections",
+		address2: "Post Office Box 24388",
+		address3: "Jackson, MS 39225",
+		address4: "",
+		law: "Mississippi Public Records Act 25-61-1 et seq.",
+		otherText: ""	
+	},
+	MO: {
+		address1: "5 South Last Chance Gulch Street",
+		address2: "P.O. Box 201301",
+		address3: "Helena, MT 59620-1301",
+		address4: "",
+		law: "Montana Public Records Act § 2-6-101 et seq.",
+		otherText: ""	
+	},
+	MT: {
+		address1: "Missouri Department of Conservation",
+		address2: "Attn: Custodian of Records",
+		address3: "P.O. Box 180",
+		address4: "Jefferson City, MO 65102",
+		law: "Sunshine Law, Mo. Rev. Stat §610.010-.035",
+		otherText: ""	
+	},
+	NE: {
+		address1: "Attn: Dawn-Renee Smith",
+		address2: "Nebraska Dept. of Correctional Services",
+		address3: "PO Box 94661",
+		address4: "Lincoln, NE 68509-4661",
+		law: "Nebraska Public Records Statutes §§ 84-712 through 84-712.09",
+		otherText: ""	
+	},
+	NV: {
+		address1: "Attn.: Family & Community services",
+		address2: "Nevada Department of Corrections",
+		address3: "P.O. Box 7011",
+		address4: "Carson City, Nevada 89702",
+		law: "Nevada Open Records Act § 239 et seq.",
+		otherText: ""	
+	},
+	NH: {
+		address1: "Jeffrey J. Lyons, Public Information Officer",
+		address2: "P.O. Box 1806",
+		address3: "105 Pleasant Street",
+		address4: "Concord, NH 03302-1806",
+		law: "Right-to-know Law RSA 91-A",
+		otherText: ""	
+	},
+	NJ: {
+		address1: "",
+		address2: "",
+		address3: "",
+		address4: "",
+		law: "",
+		otherText: "",
+		form: "http://www.state.nj.us/corrections/opra/docs/oprarequest.pdf"
+	},
+	NM: {
+		address1: "Office of General Counsel c/o General Counsel, Jim Brewster",
+		address2: "New Mexico Corrections Department",
+		address3: "PO Box 27116",
+		address4: "Santa Fe, NM 87502-0116",
+		law: "New Mexico Inspection of Public Record Act, 14-2-1 NMSA 1978 et seq.",
+		otherText: ""	
+	},
+	NY: {
+		address1: "FOIL Unit, Office of Counsel NYS Department of Corrections and Community Supervision",
+		address2: "Building 2",
+		address3: "1220 Washington Ave",
+		address4: "Albany, New York 12226-2050",
+		law: "New York Freedom of Information law, N.Y. Pub. Off. Law § 84 et seq.",
+		otherText: ""	
+	},
+	NC: {
+		address1: "Division of Prisons",
+		address2: "831 West Morgan Street",
+		address3: "4260 Mail Service Center",
+		address4: "Raleigh, NC 27699-4260",
+		law: "North Carolina Public Records Law, G.S. §132-1",
+		otherText: ""	
+	},
+	ND: {
+		address1: "Heather Kitzan, Grants and Procurement Officer II",
+		address2: "DOCR Central Office",
+		address3: "3100 E Railroad Avenue, PO Box 1898",
+		address4: "Bismarck, ND 58502-1898",
+		law: "North Dakota Open Records Statute, N.D.C.C. §44-04-18 et seq.",
+		otherText: ""	
+	},
+	OH: {
+		address1: "",
+		address2: "",
+		address3: "",
+		address4: "",
+		law: "Ohio Public Record act § 149.43 et seq.",
+		otherText: "",
+		email: "trevor.Clark@odrc.state.oh.us"
+	},
+	OK: {
+		address1: "",
+		address2: "",
+		address3: "",
+		address4: "",
+		law: "",
+		otherText: "",
+		form: "https://www.ok.gov/doc/documents/Data_Request_Form.pdf"
+	},
+	OR: {
+		address1: "",
+		address2: "",
+		address3: "",
+		address4: "",
+		law: "Oregon Public Records Law, §192.410 et seq.",
+		otherText: "",
+		email: "melissa.d.nofzinger@doc.state.or.us"
+	},
+	PA: {
+		address1: "",
+		address2: "",
+		address3: "",
+		address4: "",
+		law: "Pennsylvania Right-To-Know law, 65 P.S. § 67.101 et seq.",
+		otherText: "",
+		email: "RA-docrighttoknow@pa.gov"
+	},
+	RI: {
+		address1: "RI Department of Corrections",
+		address2: "Office of Legal Counsel",
+		address3: "40 Howard Ave.",
+		address4: "Cranston, RI 02920",
+		law: "Rhode Island Access to Public Records Act § 38-2-3",
+		otherText: ""	
+	},
+	SC: {
+		address1: "FOIA Coordinator, Office of General Counsel",
+		address2: "South Carolina Department of Corrections",
+		address3: "Post Office Box 21787",
+		address4: "Columbia, SC 29221-1787",
+		law: "South Carolina Freedom of Information Act, S.C. Code Ann § 30-4-10 et seq.",
+		otherText: ""	
+	},
+	SD: {
+		address1: "FOIA Coordinator, South Dakota Department of Corrections",
+		address2: "3200 East Highway 34, c/o 500 East",
+		address3: "Capitol Avenue",
+		address4: "Pierre, SD 57501",
+		law: "South Dakota Sunshine Law, S.D.C.L. 1-27-1 et seq.",
+		otherText: ""	
+	},
+	TN: {
+		address1: "Tennessee Department of Correction",
+		address2: "Rachel Jackson Building, Sixth Floor",
+		address3: "Nashville, Tennessee 37243-0465",
+		address4: "",
+		law: "Tennessee Open Records Act, §10-7-503 et seq.",
+		otherText: ""	
+	},
+	TX: {
+		address1: "Open Records Act Coordinator",
+		address2: "TDCJ - Executive Services",
+		address3: "P.O. Box 99",
+		address4: "Huntsville, TX 77342-0099",
+		law: "Texas Public Information Act Chapter 552.001 et. seq.",
+		otherText: ""	
+	},
+	UT: {
+		address1: "14717 Minuteman Drive",
+		address2: "Draper, UT 84020",
+		address3: "",
+		address4: "",
+		law: "Utah Government Records Access and Management Act, §63-2-101 et seq.",
+		otherText: ""	
+	},
+	VT: {
+		address1: "David Turner, Public Records Officer",
+		address2: "Vermont Department of Corrections",
+		address3: "NOB 2 South 280 State Drive",
+		address4: "Waterbury, VT 05671-2000",
+		law: "Statute V.S.A. §§ 315-320",
+		otherText: ""	
+	},
+	VA: {
+		address1: "Central Criminal Records Manager, Virginia Department of Corrections",
+		address2: "P.O. Box 26963",
+		address3: "6900 Atmore Drive",
+		address4: "Richmond, Virginia 23261",
+		law: "Freedom of Information Act, § 2.2-3700 et seq.",
+		otherText: ""	
+	},
+	WA: {
+		address1: "Department of Corrections",
+		address2: "Public Records Office",
+		address3: "P.O. Box 41118",
+		address4: "Olympia, WA 98504-1118",
+		law: "Washington Public Records Act, §42.56 et seq.",
+		otherText: ""	
+	},
+	WV: {
+		address1: "FOIA Coordinator",
+		address2: "West Virginia Department of Corrections",
+		address3: "1409 Greenbrier Street",
+		address4: "Charleston, WV 25311",
+		law: "West Virginia Freedom of Information Act W. Va. Code § 29B-1-1 et seq.",
+		otherText: ""	
+	},
+	WI: {
+		address1: "FOIA Coordinator",
+		address2: "Wisconsin Department of Corrections",
+		address3: "3099 E. Washington Ave.",
+		address4: "P.O. Box 7925, Madison, WI 53717-7925",
+		law: "Wisconsin Public Records Law, § 19.31 et seq.",
+		otherText: ""	
+	},
+	WY: {
+		address1: "Mr. Robert O. Lampert, Wyoming Department of Corrections",
+		address2: "Unicover Building, Suite 100",
+		address3: "1934 Wyott Drive",
+		address4: "Cheyenne, WY 82002-3427",
+		law: "Wyo. Stat. § 16-4-201 et seq.",
+		otherText: ""
+	}
+};
+
+function foiTemplate(state) {
+
+	var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+	n = new Date();
+	y = n.getFullYear();
+	m = monthNames[n.getMonth()]
+	d = n.getDate();
+	dateString = m + " " + d + ", " + y;
+
+	addressString = state.email ? "" : (
+				"<div>"
+				 + "<p>"
+					 + "<div> <textarea placeholder='Your Name'></textarea> </div>" 
+					 + "<div> <textarea placeholder='Street Address'></textarea> </div>" 
+					 + "<div> <textarea placeholder='City, State, Zipcode'></textarea> </div>" 
+				 + "</p>"
+				 ) 
+
+	htmlString = 
+
+				 addressString
+
+				 + "<p>"
+					 + "<div>" + dateString + "</div>"
+				 + "</p>"
+				
+				 + "<p>"	
+					 + "<div>" + state.address1 + "</div>" 
+					 + "<div>" + state.address2 + "</div>" 
+					 + "<div>" + state.address3 + "</div>" 
+					 + "<div>" + state.address4 + "</div>" 
+				 + "</p>"
+
+				 + "<p>" + "To Whom It May Concern:" + "</p>"
+
+				 + "<p>" + "Under the "
+					+ state.law + ", " 
+					+ "I am requesting an opportunity to inspect or obtain copies of public records that "
+					+ "<span>"
+						+ "<textarea style='width: 725px; height: 50px;' placeholder='Describe the records or information sought with enough detail for the public agency to respond.  Be as specific as your knowledge of the available records will allow. But it is more important to describe the information you are seeking.'></textarea>" 
+					+ "</span>"
+					+ "If there are any fees for searching or copying these records, please inform me if the cost will exceed <textarea style='width: 55px; height: 15px;' placeholder='$______'></textarea> &nbsp;&nbsp;"
+					+ "However, I would also like to request a waiver of all fees in that the disclosure of the requested information is in the public interest and will contribute significantly to the public’s understanding of "
+					+ "<span>"
+						+ "<textarea style='width: 725px; height: 33px' placeholder='Fill in what this will contribute to'></textarea>"
+					+ "</span>"
+					+ "<div style='margin-bottom: 10px'>"
+						+ "<textarea style='width: 725px; height: 33px' placeholder='Here, you can identify yourself as a representative of the news media if applicable and state that your request is related to news gathering purposes. '></textarea>"
+					+ "</div>"
+					+ "This information is not being sought for commercial purposes. "
+
+					+ "<p>"
+					  + state.otherText
+					+ "</p>"
+				 + "</p>"
+
+				 + "<p>"
+				  + "Sincerely, "
+				  + "<div>"
+				 	 + "<textarea placeholder='Your Name'></textarea>"
+				  + "</div>"
+				  + "<div>"
+				 	 + "<textarea placeholder='Your Phone Number'></textarea>"
+				 + "</div>"
+				 + "<button onclick='convertToPrintable()'>Convert to Printable Format</button>"
+	     + "</div>"
+
+	return htmlString;
+}
+
+function convertToPrintable() {
+	$('textarea').each(function() {
+		$(this).replaceWith($(this).val())
+	})
+}
+
+$(document).ready(function() {
+	for (state in foiInfo) {
+		if (foiInfo[state].form) {
+			htmlString = "<div>This state uses an online form to file Freedom of Information requests. "
+			+ "Use <a href='" + foiInfo[state].form + "'>this</a> site to file the request."
+			+ "</div>"
+			$('#' + state + " ~ .state-info-p").html(htmlString);
+		} else {
+			$('#' + state + " ~ .state-info-p").html(foiTemplate(foiInfo[state]));
+		}
+	}
+})
